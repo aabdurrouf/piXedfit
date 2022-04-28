22ad9e4f7905f4300ae8ff3d9b60b280e7c73aac
ABDURROUF <abdurrouf@ABDURROUFs-MacBook-Pro.local>
2022-04-28 08:04:28 +0800

    updates

--- piXedfit/piXedfit_images/images_utils.py
+++ piXedfit/piXedfit_images/images_utils.py
@@ -7,12 +7,9 @@ from astropy.cosmology import *
 from ..utils.filtering import cwave_filters
 
 
-__all__ = ["sort_filters", "kpc_per_pixel", "k_lmbd_Fitz1986_LMC", "EBV_foreground_dust", "skybg_sdss", "get_gain_dark_variance",
-			"var_img_sdss", "var_img_GALEX", "var_img_2MASS", "var_img_WISE", "var_img_from_unc_img",
-			"var_img_from_weight_img", "segm_sextractor", "mask_region_bgmodel", "subtract_background", "get_psf_fwhm",
-			"get_largest_FWHM_PSF", "ellipse_fit", "draw_ellipse", "ellipse_sma", "crop_ellipse_galregion",
-			"crop_ellipse_galregion_fits", "crop_stars", "crop_stars_galregion_fits",  "crop_image_given_radec", 
-			"segm_sep", "crop_image_given_xy", "check_avail_kernel", "create_kernel_gaussian",
+__all__ = ["sort_filters", "kpc_per_pixel", "k_lmbd_Fitz1986_LMC", "EBV_foreground_dust", "skybg_sdss", "get_gain_dark_variance", "var_img_sdss", "var_img_GALEX", "var_img_2MASS", "var_img_WISE", "var_img_from_unc_img",
+			"var_img_from_weight_img", "mask_region_bgmodel", "subtract_background", "get_psf_fwhm","get_largest_FWHM_PSF", "ellipse_fit", "draw_ellipse", "ellipse_sma", "crop_ellipse_galregion",
+			"crop_ellipse_galregion_fits", "crop_stars", "crop_stars_galregion_fits",  "crop_image_given_radec", "segm_sep", "crop_image_given_xy", "check_avail_kernel", "create_kernel_gaussian",
 			"raise_errors", "get_img_pixsizes", "in_kernels", "get_flux_or_sb"]
 
 
