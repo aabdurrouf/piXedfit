e5259c9c52cef9aef5b04de82b8f9be02afb7772
ABDURROUF <abdurrouf@ABDURROUFs-MacBook-Pro.local>
2022-07-14 20:29:26 +0800

    update examples

--- examples/FUVtoNIR_CALIFA/script_match_imgifs_spectral.py
+++ examples/FUVtoNIR_CALIFA/script_match_imgifs_spectral.py
@@ -1,12 +1,6 @@
-import os, sys
-
-global PIXEDFIT_HOME
-PIXEDFIT_HOME = os.environ['PIXEDFIT_HOME']
-sys.path.insert(0, PIXEDFIT_HOME)
-
 from piXedfit.piXedfit_spectrophotometric import match_imgifs_spectral
 
 specphoto_file = "specphoto_fluxmap_ngc309.fits"
+nproc = 10
 name_out_fits = "corr_%s" % specphoto_file
-
-match_imgifs_spectral(specphoto_file, nproc=20, name_out_fits=name_out_fits)
+match_imgifs_spectral(specphoto_file, nproc=nproc, name_out_fits=name_out_fits)
