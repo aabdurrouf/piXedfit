4f1a58a411df8260de629b649d16379ac97b6544
Abdurro'uf <fabdurr1@jhu.edu>
2025-07-07 14:46:59 -0400

    Update redshifting.py

--- piXedfit/utils/redshifting.py
+++ piXedfit/utils/redshifting.py
@@ -2,11 +2,46 @@ import numpy as np
 import math
 import sys, os
 from astropy.cosmology import *
+from astropy.constants import L_sun
 
 __all__ = ["cosmo_redshifting"]
 
+def cosmo_redshifting(wave=[], spec=[], z=0.01, cosmo='flat_LCDM', H0=70.0, Om0=0.3, DL_Gpc=0.0):
+    if DL_Gpc > 0.0:
+        D_L_cm = DL_Gpc*3.08568e+27    # in cm
+    else:
+        if cosmo=='flat_LCDM' or cosmo==0:
+            cosmo1 = FlatLambdaCDM(H0=H0, Om0=Om0)
+            DL = cosmo1.luminosity_distance(z)      # in unit of Mpc
+        elif cosmo=='WMAP5' or cosmo==1:
+            DL = WMAP5.luminosity_distance(z)
+        elif cosmo=='WMAP7' or cosmo==2:
+            DL = WMAP7.luminosity_distance(z)
+        elif cosmo=='WMAP9' or cosmo==3:
+            DL = WMAP9.luminosity_distance(z)
+        elif cosmo=='Planck13' or cosmo==4:
+            DL = Planck13.luminosity_distance(z)
+        elif cosmo=='Planck15' or cosmo==5:
+            DL = Planck15.luminosity_distance(z)
+        
+        D_L_cm = DL.value*3.08568e+24
 
-def cosmo_redshifting(DL_Gpc=0.0,cosmo='flat_LCDM',H0=70.0,Om0=0.3,z=0.01,wave=[],spec=[]):
+    # Observed wavelength (redshifted)
+    wave_obs = wave * (1 + z)
+
+    # Convert luminosity to erg/s/Angstrom
+    L_lambda_erg = spec * L_sun.to('erg/s').value  # Now in erg/s/Angstrom
+
+    # Get luminosity distance in cm
+    D_L_cm = cosmo.luminosity_distance(z).to('cm').value
+
+    # Compute observed flux density using:
+    # F_lambda = (1 / (4 * pi * D_L^2)) * (1 / (1 + z)) * L_lambda_rest
+    F_lambda_obs = (L_lambda_erg / (4 * np.pi * D_L_cm**2)) / (1 + z)  # in erg/s/cm^2/Angstrom
+
+    return wave_obs, F_lambda_obs
+
+def cosmo_redshifting_old(DL_Gpc=0.0,cosmo='flat_LCDM',H0=70.0,Om0=0.3,z=0.01,wave=[],spec=[]):
     """
     :param DL_Gpc (default: 0.0):
         Luminosity distance (LD) in unit of Gpc. If this parameter is not zero, the LD
